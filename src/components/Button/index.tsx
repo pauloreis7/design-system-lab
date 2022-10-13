df7837c7c0bf02e084616516e1a736928df0714b
Paulo Reis <paulosilvadosreis2057@gmail.com>
2022-10-13 09:19:46 -0300

    feat: create Button component and stories

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,23 @@
+import { ButtonHTMLAttributes, ReactNode } from 'react'
+import { clsx } from 'clsx'
+import { Slot } from '@radix-ui/react-slot'
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode
+  asChild?: boolean
+}
+
+export function Button({ asChild, children, ...props }: ButtonProps) {
+  const Comp = asChild ? Slot : 'button'
+
+  return (
+    <Comp
+      className={clsx(
+        'py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white'
+      )}
+      {...props}
+    >
+      {children}
+    </Comp>
+  )
+}
